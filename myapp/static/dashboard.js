b395d8159c62907ee22d90d3c88f8238831fc3a7
Connor <on3s1ngular1ty@gmail.com>
2024-07-31 19:07:44 -0400

    Initialized variable in dashboard.js

---
+++ myapp/static/dashboard.js
@@ -0,0 +1,19 @@
+
+window.addEventListener('load', function(){
+
+    let rent = document.getElementById("rent");
+    let groceries = document.getElementById("groceries");
+    let other = document.getElementById("other");
+
+    let emergency = document.getElementById("emergency");
+    let retirement = document.getElementById("retirement");
+    let debt = document.getElementById("debt");
+
+    let vacation = document.getElementById("vacation"); 
+    let clothing = document.getElementById("clothing");
+    let other2 = document.getElementById("other2");
+
+
+
+
+})
