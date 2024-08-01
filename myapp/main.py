f0c425da348b91adea512e99aaf2f018b266bb5f
Connor <on3s1ngular1ty@gmail.com>
2024-08-01 11:31:53 -0400

    started logic for backend on dashboard route

--- myapp/main.py
+++ myapp/main.py
@@ -5,7 +5,7 @@ main = Blueprint('main', __name__)
 
 from .helpers import taxCalculator, budgetOptions, ny_tax_brackets, nyc_tax_brackets, federal_brackets
 from werkzeug.security import check_password_hash, generate_password_hash
-from .models import db, User, Stats
+from .models import db, User, Stats, Necessities, Savings, Wants
 
         
 @main.route("/")
@@ -36,10 +36,35 @@ def dashboard():
                 "code": "red"
             }), 200)
             return res"""
-
-
-
         return render_template("dashboard.html")
+    else:
+        req = request.get_json()
+        vars = req['vars']
+        type = req['type']
+        if type == "necessities":
+            rent1 = vars[0]
+            groceries1 = vars[1]
+            other1 = vars[2]
+            check = Necessities.query.get(current_user.id)
+            if check is None:
+                new_instance = Necessities(rent=rent1, groceries=groceries1, other=other1)
+                db.session.add(new_instance)
+                db.session.commit()
+            else:
+                check.rent = rent1
+                check.groceries = groceries1
+                check.other = other1
+                db.session.commit()
+        elif type == "savings":
+            emergency1 = vars[0]
+            retirement1 = vars[1]
+            debt1 = vars[2]
+            Savings(emergency=emergency1, retirement=retirement1, debt=debt1)
+        else: 
+            #nothing for now 
+            return
+
+        
 
 
 
